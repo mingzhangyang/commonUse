a6d3e942dfbf5b6e6ffbb862b22fc03b21fc4ad6
yangm11 <mingzhang.yang@nih.gov>
2017-06-07 10:13:55 -0400

    exports optimization

---
+++ array/customFuncsOnArray.js
@@ -0,0 +1,33 @@
+/**
+ * Created by mingzhang on 6/7/17.
+ */
+'use strict';
+
+const customFuncsOnArray = (function () {
+  function map(a, f) {
+    let res = [];
+    for (let i = 0; i < a.length; i++) {
+      res.push(f(a[i], i));
+    }
+  }
+
+  function filter(a, f) {
+    let res = [];
+    let t;
+    for (let i = 0; i < a.length; i++) {
+      t = f(a[i], i);
+      if (t) {
+        res.push(a[i]);
+      }
+    }
+  }
+
+  return {
+    map: map,
+    filter: filter
+  }
+})();
+
+if (typeof module !== 'undefined' && module.parent) {
+  module.exports = customFuncsOnArray;
+}
