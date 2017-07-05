3f4e1b7104d657d0d0db9ee25cc22c19d9a57991
yangm11 <mingzhang.yang@nih.gov>
2017-07-05 18:09:08 -0400

    merge into one file of deepLearning.ts

---
+++ statistics/deepLearning.ts
@@ -0,0 +1,53 @@
+/**
+ * Created by yangm11 on 7/5/2017.
+ */
+
+interface Array<T> {
+    fill(value: T): Array<T>;
+}
+
+function softMax(arr: number[]) {
+    let sum: number = 0;
+    let t: number[] = [];
+    for (let i = 0; i < arr.length; i++) {
+        let tmp: number = Math.pow(Math.E, arr[i]);
+        sum += tmp;
+        t.push(tmp);
+    }
+    let res: number[] = [];
+    for (let i = 0; i < t.length; i++) {
+        res.push(t[i] / sum);
+    }
+    return res;
+}
+
+function oneHot(arr: number[]) {
+    let max = -Infinity;
+    for (let i = 0; i < arr.length; i++) {
+        if (max < arr[i]) {
+            max = arr[i];
+        }
+    }
+    let res: number[][] = [];
+    for (let i = 0; i < arr.length; i++) {
+        let r: number[] = new Array(max + 1);
+        r.fill(0);
+        r[arr[i]] = 1;
+        res.push(r);
+    }
+    return res;
+}
+
+function crossEntropy(arr1: number[], arr2: number[]): number {
+    if (arr1.length !== arr2.length) {
+        throw 'Two arrays with same length are expected';
+    }
+    let sum: number = 0;
+    for (let i = 0; i < arr1.length; i++) {
+        sum += arr1[i] * Math.log(arr2[i]);
+    }
+    return -sum;
+}
+
+console.log(softMax([3, 1, 0.2]));
+console.log(softMax([1, 2, 3]));
