971c8be6ffab0fee5becdd8dba6fc602bc43e968
Mingzhang <redondoself@gmail.com>
2017-07-25 01:15:15 -0400

    Create event.js
    event.js

---
+++ playground/event.js
@@ -0,0 +1,37 @@
+'use strict'
+
+class EventEmitter {
+  constructor(name) {
+    this.eventName = name;
+    this.target = [];
+    
+  }
+
+  emit() {
+    for (let i = 0; i < this.target.length; i++) {
+      this.target[i]._events[this.eventName] += 1;
+    }
+  }
+}
+
+class EventListener {
+  constructor() {
+    Object.defineProperty(this, '_events', {
+      configurable: true,
+      enumerable: false,
+      value: {},
+      writable: false
+    });
+  }
+
+  on(eventName, eventEmitter, handler) {
+    eventEmitter = eventEmitter || new EventEmitter();
+    eventEmitter.target.push(this);
+    Object.defineProperty(this._events, eventName, {
+      configurable: true,
+    set: (v) => {
+      setTimeout(handler, 0);
+      }
+    });
+  }
+}
