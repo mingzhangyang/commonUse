d11b32ef65e2f61aa826d2ee2bf4b0a89aae245a
yangm11 <mingzhang.yang@nih.gov>
2017-08-04 18:59:19 -0400

    mapLine.js upgraded

--- files/mapLine.js
+++ files/mapLine.js
@@ -8,20 +8,55 @@ const readline = require('readline');
 const os = require('os');
 
 let mapOptions = {
-  'upperCase': str => str.toUpperCase(),
-  'lowerCase': str => str.toLowerCase(),
-  'capitalize': str => {
-    if (str.length > 0) {
-      return str[0].toUpperCase() + str.slice(1, -1);
-    }
-    return '';
-  },
-  'tidy': str => {
-    if (str.trim() === '') {
+    'upperCase': str => str.toUpperCase(),
+    'lowerCase': str => str.toLowerCase(),
+    'capitalize': str => {
+      if (str.length > 0) {
+        return str[0].toUpperCase() + str.slice(1, -1);
+      }
+      return '';
+    },
+    'tidy': str => {
+      if (str.trim() === '') {
+        return null;
+      }
+      return str;
+    },
+    'addLineNumber': (str, index) => (index + 1) + '.\t' + str,
+    'csv2tsv': str => str.replace(/,/g, '\t'),
+    'tsv2csv': str => str.replace(/\t/g, ','),
+    'reverse': str => {
+      str = str.split('').reverse();
+      return str.join('');
+    },
+    'GC_content': (str, index, rl) => {
+      if (!rl['GC_content']) {
+        rl['GC_content'] = {GC: 0, total: 0};
+      }
+      if (!rl['GC_content_calculator']) {
+        rl['GC_content_calculator'] = line => {
+          line = line.trim();
+          if (line[0] !== '>') {
+            for (let i = 0; i < line.length; i++) {
+              if ('gcGC'.includes(line[i])) {
+                rl['GC_content'].GC += 1;
+              }
+              rl['GC_content'].total += 1;
+            }
+          }
+        }
+      }
+      rl['GC_content_calculator'](str);
+      if (!rl._lastCall) {
+        rl._lastCall = () => {
+          console.log(rl['GC_content']);
+          console.log('GC_content: ' + (rl['GC_content'].GC / rl['GC_content'].total * 100).toFixed(2) + '%');
+        }
+      }
       return null;
     }
-    return str;
-  }
+
+
 
 };
 
@@ -30,7 +65,7 @@ let mapOptions = {
  * @param path: the path to the file to be processed
  * @param func: either a string or a custom function
  *    if func is a string, pre-defined function will be invoked if the string matches one of the names of pre-defined functions;
- *    if func is a function, make sure the return value is the expected string that will be written to the output. The parameters that the function received will be the line (string) and its line number (int) which starts from 0. If you want to omit a line, please make the return value of that line be 'null'.
+ *    if func is a function, make sure the return value is the expected string that will be written to the output. The parameters that the function received will be the line (string) and its line number (int) which starts from 0, the readline interface and the output write stream. If you want to omit a line, please make the return value of that line be 'null'.
  */
 function mapLine(path, func) {
   if (typeof func === 'string') {
@@ -48,11 +83,12 @@ function mapLine(path, func) {
   rl.outputLineCount = 0;
   rl.on('line', line => {
     let toWrite;
-    try {
-      toWrite = func(line, rl.lineIndex++);
-    } catch (err) {
-      throw 'Failed to process @ ' + rl.lineIndex + ': ' + line;
-    }
+    // try {
+    //   toWrite = func(line, rl.lineIndex++, rl, output);
+    // } catch (err) {
+    //   throw 'Failed to process @ ' + rl.lineIndex + ': ' + line;
+    // }
+    toWrite = func(line, rl.lineIndex++, rl, output);
     if (toWrite !== null) {
       output.write(toWrite + os.EOL);
       rl.outputLineCount++;
@@ -60,9 +96,15 @@ function mapLine(path, func) {
   });
 
   rl.on('close', () => {
-    console.log('Done!');
-    console.log('Total number of lines processed: ' + (rl.lineIndex + 1));
-    console.log('Total number of lines output: ' + (rl.outputLineCount + 1));
+    output.end();
+    setImmediate(() => {
+      if (rl._lastCall) {
+        rl._lastCall();
+      }
+      console.log('Done!');
+      console.log('Total number of lines processed: ' + (rl.lineIndex + 1));
+      console.log('Total number of lines output: ' + (rl.outputLineCount + 1));
+    });
   })
 }
 
