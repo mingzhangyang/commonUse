c3eb352cea28ec3facd017747cd7cd51b452b092
yangm11 <mingzhang.yang@nih.gov>
2017-08-11 16:57:06 -0400

    obj2elem

---
+++ web/obj2elem/obj2elem.js
@@ -0,0 +1,57 @@
+/**
+ * Created by yangm11 on 8/11/2017.
+ */
+'use strict';
+
+function obj2elems(obj, parentNode, tagName = 'div') {
+  parentNode = parentNode || document.body;
+  let div = document.createElement('div');
+  let subParent = parentNode.appendChild(div);
+  let keys = Object.keys(obj);
+  for (let i = 0; i < keys.length; i++) {
+    let cur = obj[keys[i]];
+    let child = document.createElement('div');
+    child.textContent = (keys[i] + ': ');
+    subParent.append(child);
+    switch (typeof cur) {
+      case 'object':
+        if (Array.isArray(cur)) {
+          array2elems(cur, child, tagName);
+        } else {
+          obj2elems(cur, child, tagName);
+        }
+        break;
+      default:
+        value2element(cur, child, 'input');
+    }
+  }
+}
+
+function array2elems(array, parentNode, tagName = 'div') {
+  parentNode = parentNode || document.body;
+  let div = document.createElement('div');
+  let subParent = parentNode.appendChild(div);
+  for (let i = 0; i < array.length; i++) {
+    let cur = array[i];
+    switch (typeof cur) {
+      case 'object':
+        if (Array.isArray(cur)) {
+          array2elems(cur, subParent, tagName);
+        } else {
+          obj2elems(cur, subParent, tagName);
+        }
+        break;
+      default:
+        let child = document.createElement('div');
+        subParent.append(child);
+        value2element(cur, child, 'input');
+    }
+  }
+}
+
+function value2element(v, parentNode, tagName = 'input') {
+  parentNode = parentNode || document.body;
+  let child = document.createElement(tagName);
+  child.placeholder = v;
+  parentNode.appendChild(child);
+}
