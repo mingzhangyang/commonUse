b6f9d51e7e8bb59b852a2fc21c010da5eaf64734
yangm11 <mingzhang.yang@nih.gov>
2017-08-23 18:26:44 -0400

    substr vs sbuseq

---
+++ string/editDistance.js
@@ -0,0 +1,4 @@
+/**
+ * Created by yangm11 on 8/23/2017.
+ */
+'use strict';
--- string/extendedMethods.js
+++ string/extendedMethods.js
@@ -3,7 +3,7 @@
  */
 'use strict';
 
-function findAllSubstrWitnGivenLength(str, n) {
+function findAllSubstringWitnGivenLength(str, n) {
   let res = [];
   let i = 0;
   while (i < str.length - n + 1) {
@@ -14,4 +14,22 @@ function findAllSubstrWitnGivenLength(str, n) {
     i++;
   }
   return res;
+}
+
+function findAllSubsequenceWIthGivenLength(str, n) {
+  let res = [];
+  function worker(s, len) {
+    let tmp = [];
+    if (len === 1) {
+      for (let i = 0; i < s.length; i++) {
+        if (!tmp.includes(s[i])) {
+          tmp.push(s[i]);
+        }
+      }
+      return tmp;
+    }
+    for (let k = 1; k < s.length - len + 1; k++) {
+      let cur = [s[k - 1]].concat(findAllSubsequenceWIthGivenLength())
+    }
+  }
 }
