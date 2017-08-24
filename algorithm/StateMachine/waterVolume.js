276601ed5cfb4191f8f822c5aa85a306d13713ce
yangm11 <mingzhang.yang@nih.gov>
2017-08-24 12:40:42 -0400

    trapping rain water

--- algorithm/StateMachine/waterVolume.js
+++ algorithm/StateMachine/waterVolume.js
@@ -3,7 +3,20 @@
  */
 'use strict';
 
+/*
+ https://leetcode.com/problems/trapping-rain-water/description/
+
+ Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+
+ For example,
+ Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
+
+ */
+
 function wv(arr) {
+  if (arr.length < 3) {
+    return 0;
+  }
   let res = 0;
   let state = {
     left: 0,
@@ -30,46 +43,100 @@ function wv(arr) {
     if (state.left > 0 && state.right === 0 && state.content.length > 0) {
       if (cur >= state.left) {
         state.right = cur;
+        for (let i = 0; i < state.content.length; i++) {
+          res += (state.left - state.content[i]);
+        }
+        state.left = state.right;
+        state.content = [];
+        state.right = 0;
       } else {
         state.content.push(cur);
       }
       i++;
       continue;
     }
-    if (state.left > 0 && state.right > 0 && state.content.length > 0) {
-      for (let i = 0; i < state.content.length; i++) {
-        res += (state.left - state.content[i]);
-      }
-      if (cur >= state.right) {
-        state.left = cur;
-        state.content = [];
-        state.right = 0;
-      } else {
-        state.content = [cur];
-        state.right = 0;
-      }
-      i++;
-      continue;
-    }
-    console.log('missing something ...');
+    console.log('If you see this, there must be something missed...');
   }
   if (state.right === 0 && state.content.length === 0) {
     return res;
   }
   if (state.left > 0 && state.content.length > 0) {
-    let max = 0;
-    for (let i = 0; i < state.content.length; i++) {
-      if (state.content[i] > max) {
-        max = state.content[i];
-      }
-    }
-    if (max === 0) {
-      return res;
-    } else {
-
-
-      // ....
-    }
-
+    let _arr = state.content;
+    _arr.reverse();
+    _arr.push(state.left);
+    console.log('reverse the remaining, and start recursion.');
+    return res + wv(_arr);
   }
 }
+
+let a = [5, 2, 1, 2, 1, 5];
+console.log(wv(a));
+
+
+/*
+  Alternative solution (more elegant?)
+  The idea comes from mimic filling water into reservoir.
+  It is more straightforward. But if the numbers distribute too far,
+  this method will be less efficient.
+ */
+
+function _wv(arr) {
+  let res = 0;
+  let max = 0, min = +Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+
+  arr = arr.map(d => d - min);
+  max -= min;
+  let matrix = new Array(max);
+
+  for (let i = 0; i < max; i++) {
+    let row = new Array(arr.length);
+    for (let j = 0; j < arr.length; j++) {
+      row[j] = (arr[j] > i) ? 1 : 0;
+    }
+    matrix[i] = row;
+  }
+
+  function sum(a) {
+    return a.reduce((acc, cur) => acc + cur, 0);
+  }
+
+  for (let i = 0; i < max; i++) {
+    let row = matrix[i];
+    if (sum(row) < 2) {
+      continue;
+    }
+    for (let j = 1; j < row.length - 1; j++) {
+      let v = row[j];
+      if (v === 0) {
+        let flag1, flag2;
+        for (let k = j - 1; k > -1; k--) {
+          if (row[k] === 1) {
+            flag1 = true;
+            break;
+          }
+        }
+        for (let h = j + 1; h < row.length; h++) {
+          if (row[h] === 1) {
+            flag2 = true;
+            break;
+          }
+        }
+        if (flag1 && flag2) {
+          res += 1;
+        }
+      }
+    }
+  }
+
+  return res;
+}
+
+console.log(_wv(a));
---
+++ playground/virtualSpace.js
@@ -0,0 +1,33 @@
+/**
+ * Created by yangm11 on 8/24/2017.
+ */
+'use strict';
+
+/*
+  1. a 1-d array of integers can be turned to a 2-d array with 1 and 0.
+    eg. [1, 3, 0, 2] can be turned to
+    [
+      [1, 1, 0, 1],
+      [0, 1, 0, 1],
+      [0, 1, 0, 0]
+    ]
+  2. use a 3-d array of objects to mimic the space we live in.
+    e.g.
+      [
+        [
+          [obj, obj, obj],
+          [obj, 0bj, obj],
+          [0bj, obj, obj
+        ],
+        [
+          [obj, obj, obj],
+          [obj, 0bj, obj],
+          [0bj, obj, obj
+        ],
+        [
+          [obj, obj, obj],
+          [obj, 0bj, obj],
+          [0bj, obj, obj
+        ]
+      ]
+ */
