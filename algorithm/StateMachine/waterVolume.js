bd1352ca34249c53aa0b05bab5e83aab73b3ab5a
Mingzhang <redondoself@gmail.com>
2017-08-24 08:37:19 -0400

    morning work

--- algorithm/StateMachine/waterVolume.js
+++ algorithm/StateMachine/waterVolume.js
@@ -2,3 +2,74 @@
  * Created by mingzhang on 8/23/17.
  */
 'use strict';
+
+function wv(arr) {
+  let res = 0;
+  let state = {
+    left: 0,
+    right: 0,
+    content: []
+  };
+  let i = 0;
+  while (i < arr.length) {
+    let cur = arr[i];
+    if (state.left === 0 && state.right === 0 && state.content.length === 0) {
+      state.left = cur;
+      i++;
+      continue;
+    }
+    if (state.left > 0 && state.right === 0 && state.content.length === 0) {
+      if (cur < state.left) {
+        state.content.push(cur);
+      } else {
+        state.left = cur;
+      }
+      i++;
+      continue;
+    }
+    if (state.left > 0 && state.right === 0 && state.content.length > 0) {
+      if (cur >= state.left) {
+        state.right = cur;
+      } else {
+        state.content.push(cur);
+      }
+      i++;
+      continue;
+    }
+    if (state.left > 0 && state.right > 0 && state.content.length > 0) {
+      for (let i = 0; i < state.content.length; i++) {
+        res += (state.left - state.content[i]);
+      }
+      if (cur >= state.right) {
+        state.left = cur;
+        state.content = [];
+        state.right = 0;
+      } else {
+        state.content = [cur];
+        state.right = 0;
+      }
+      i++;
+      continue;
+    }
+    console.log('missing something ...');
+  }
+  if (state.right === 0 && state.content.length === 0) {
+    return res;
+  }
+  if (state.left > 0 && state.content.length > 0) {
+    let max = 0;
+    for (let i = 0; i < state.content.length; i++) {
+      if (state.content[i] > max) {
+        max = state.content[i];
+      }
+    }
+    if (max === 0) {
+      return res;
+    } else {
+
+
+      // ....
+    }
+
+  }
+}
--- string/extendedMethods.js
+++ string/extendedMethods.js
@@ -21,7 +21,38 @@ function isSubstr(str, sub) {
 }
 
 function isSubseq(str, sub) {
-  
+  let i = 0, j = 0;
+  while (i < str.length && j < sub.length) {
+    if (str[i] === sub[j]) {
+      j++;
+    }
+    i++;
+  }
+  return j === sub.length;
 }
 
+function findAllSubsequenceWithGivenLength(str, n) {
+  let res = [];
+  if (n === 1) {
+    for (let i = 0; i < str.length; i++) {
+      if (!res.includes(str[i])) {
+        res.push(str[i]);
+      }
+    }
+    return res;
+  }
+  for (let i = 1; i < str.length - (n - 1) + 1; i++) {
+    let cur = findAllSubsequenceWithGivenLength(str.slice(i), n - 1);
+    for (let j = 0; j < cur.length; j++) {
+      if (!res.includes(str[i - 1] + cur[j])) {
+        res.push(str[i - 1] + cur[j]);
+      }
+    }
+  }
+  return res;
+}
+
+let s = 'atgctcgtagctgcatgtcaatcg';
+console.log(findAllSubsequenceWithGivenLength(s, 3).length);
+console.log(isSubseq(s, 'actu'));
 
