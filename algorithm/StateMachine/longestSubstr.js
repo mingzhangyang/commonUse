3cd7a29ae0a2aaac1c40c6a9dd6be6b25e07f657
yangm11 <mingzhang.yang@nih.gov>
2017-09-26 11:54:20 -0400

    longestSubstr.js

---
+++ algorithm/StateMachine/longestSubstr.js
@@ -0,0 +1,48 @@
+/**
+ * Created by yangm11 on 9/26/2017.
+ */
+'use strict';
+
+function longestSubstr(arr) {
+  let st = {
+    start: 0,
+    maxVal: -1,
+    values: [],
+    maxLen: 0,
+    candidates: []
+  };
+
+  while (st.start < arr.length) {
+    if (arr[st.start] > st.maxVal) {
+      st.maxVal = arr[st.start];
+      st.values.push(arr[st.start]);
+    } else {
+      if (st.values.length > st.maxLen) {
+        st.maxLen = st.values.length;
+        st.candidates = [st.values.slice()];
+      } else if (st.values.length === st.maxLen) {
+        st.candidates.push(st.values.slice());
+      }
+      st.maxVal = arr[st.start];
+      st.values = [arr[st.start]];
+    }
+    st.start += 1;
+  }
+  if (st.values.length > st.maxLen) {
+    st.maxLen = st.values.length;
+    st.candidates = [st.values.slice()];
+  } else if (st.values.length === st.maxLen) {
+    st.candidates.push(st.values.slice());
+  }
+  return st;
+}
+
+
+if (typeof module !== 'undefined' && module.parent) {
+
+} else {
+  // test code go here
+  let a = [1, 2, 4, 2, 1, 3, 5, 7, 2, 6, 3, 5, 7, 8, 9, 2, 3, 1, 5, 7, 8, 10, 11];
+  console.log(a.length);
+  console.log(longestSubstr(a));
+}
