30a23834c9e8d644dcf131391177589609e06c70
yangm11 <mingzhang.yang@nih.gov>
2017-11-10 03:11:00 -0500

    testing

--- playground/fastLookup.js
+++ playground/fastLookup.js
@@ -5,7 +5,7 @@
 
 function randomString() {
   var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
   for( var i=0; i < 5; i++ )
     text += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -20,27 +20,51 @@ let a = [];
 function run(n) {
   let start = Date.now();
   for (let i = 0; i < n; i++) {
-    m.set(randomString(), randomString());
+    let s = randomString();
+    if (m.has(s)) {
+      m.get(s).push(randomString());
+    } else {
+      m.set(s, [randomString()]);
+    }
   }
   console.log('Map set, ' + (Date.now() - start) + ' ms');
 
   start = Date.now();
   for (let i = 0; i < n; i++) {
-    o[randomString()] = randomString();
+    let s = randomString();
+    if (o[s]) {
+      o[s].push(randomString());
+    } else {
+      o[s] = [randomString()];
+    }
   }
   console.log('Object set, ' + (Date.now() - start) + ' ms');
 
   start = Date.now();
   for (let i = 0; i < n; i++) {
-    a.push([randomString(), randomString()]);
+    let s = randomString();
+    if (i === 0) {
+      a.push([s, randomString()]);
+    } else {
+      for (let j = 0; j < a.length; j++) {
+        if (a[j][0] === s) {
+          a[j].push(randomString());
+          break;
+        }
+        if (j === a.length - 1) {
+          a.push([s, randomString()]);
+        }
+      }
+    }
+
   }
   console.log('Array set, ' + (Date.now() - start) + ' ms');
 
   let s = randomString();
 
   start = Date.now();
-  for (let key of m) {
-    if (m.get(key) === s) {
+  for (let [k, v] of m.keys()) {
+    if (v.includes(s)) {
       console.log('Found in map');
     }
   }
@@ -49,7 +73,7 @@ function run(n) {
   start = Date.now();
   let props = Object.keys(o);
   for (let i = 0; i < props.length; i++) {
-    if (o[props[i]] === s) {
+    if (o[props[i]].includes(s)) {
       console.log('Found in object');
     }
   }
@@ -57,11 +81,13 @@ function run(n) {
 
   start = Date.now();
   for (let i = 0; i < a.length; i++) {
-    if (a[i][1] === s) {
+    if (a[i].includes(s)) {
       console.log('Found in array');
     }
   }
   console.log('Iterate the array in ' + (Date.now() - start) + ' ms');
+  console.log(a.slice(0, 10));
+  console.log(a.length);
 }
 
 if (typeof module !== 'undefined' && module.parent) {
