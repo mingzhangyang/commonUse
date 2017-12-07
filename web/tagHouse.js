9d64bc51324a0c6751ee07cf49989d58febc177c
Mingzhang Yang <mingzhang.yang@nih.gov>
2017-12-07 11:46:52 -0500

    improvement on tagHoust.js

--- web/tagHouse.js
+++ web/tagHouse.js
@@ -9,6 +9,10 @@
  * @returns {string}
  */
 function tagHouse(obj) {
+  if (!(obj.type)) {
+    console.log('No type field found in the object');
+    return '';
+  }
   let res = '<' + obj.type;
   let props = Object.keys(obj);
   let children;
@@ -23,11 +27,30 @@ function tagHouse(obj) {
     res += ' ' + props[i] + '=' + '"' + obj[props[i]] + '"';
   }
   res += '>';
-  if (children) {
-    for (let k = 0; k < children.length; k++) {
-      res += tagHouse(obj);
-    }
+
+  switch (Object.prototype.toString.call(children)) {
+    case '[object Array]':
+      for (let k = 0; k < children.length; k++) {
+        res += tagHouse(children[k]);
+      }
+      break;
+    case '[object Object]':
+      if (children.type === 'textContent') {
+        res += children.content;
+      } else {
+        res += tagHouse(children);
+      }
+      break;
+    case '[object String]':
+      res += children;
+      break;
+    case '[object Number]':
+      res += children;
+      break;
+    default:
+      console.log('Child should be array, object, string, or number.')
   }
+
   if (['input', 'img', 'hr', 'meta', 'link'].includes(obj.type)) {
     return res;
   }
@@ -38,4 +61,15 @@ if (typeof module !== 'undefined' && module.parent) {
 
 } else {
   // test code go here
+  let d = {
+    type: 'div',
+    id: 'testid',
+    class: 'testclass',
+    child: {
+      type: 'textContent',
+      content: 'This is a test'
+    }
+  };
+
+  console.log(tagHouse(d));
 }
