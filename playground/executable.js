ac40239489761c37765d9a9447f4adbddc2288f4
Mingzhang Yang <mingzhang.yang@nih.gov>
2018-03-13 14:15:19 -0400

    convert a js script to be an executable

---
+++ playground/executable.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+/**
+ * Created by yangm11 on 3/13/2018.
+ */
+'use strict';
+
+if (typeof module !== 'undefined' && module.parent) {
+
+} else {
+  // test code go here
+  console.log('Hello world');
+}
