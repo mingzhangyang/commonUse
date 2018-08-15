d3976ff31de368025a7f1e10c330ac3c2d609d8e
Mingzhang <redondoself@gmail.com>
2018-08-14 23:25:26 -0400

    read file

---
+++ web/readFile.js
@@ -0,0 +1,14 @@
+/**
+ * Created by mingzhang on 8/14/18.
+ */
+'use strict';
+
+function readAsText(id, cb) {
+  
+}
+
+if (typeof module !== 'undefined' && module.parent) {
+  //module.exports =
+} else {
+
+} 
