af44c9d94f9b3880586bb5970aa5fdd96104e538
Mingzhang Yang <mingzhang.yang@nih.gov>
2018-10-11 15:58:22 -0400

    bTree

--- algorithm/Data_Structure/binaryTree.js
+++ algorithm/Data_Structure/binaryTree.js
@@ -83,7 +83,7 @@ class Node {
     this.right = null;
   }
   insert(v) {
-    if (this.value > v) {
+    if (this.value >= v) {
       if (this.left) {
         this.left.insert(v);
       } else {
@@ -133,6 +133,127 @@ class Node {
       [this.left, this.right] = [this.right, this.left];
     }
   }
+  allPaths() {
+    if (!this.left && !this.right) {
+      return [[this.value]];
+    }
+    if (!this.left && this.right) {
+      return this.right.allPaths().map(arr => [this.value, ...arr]);
+    }
+    if (this.left && !this.right) {
+      return this.left.allPaths().map(arr => [this.value, ...arr]);
+    }
+    if (this.left && this.right) {
+      return this.left.allPaths().map(arr => [this.value, ...arr])
+          .concat(this.right.allPaths().map(arr => [this.value, ...arr]));
+    }
+  }
+}
+
+class BiTree {
+  constructor() {
+    this.root = null;
+    this.size = 0;
+    this.reversed = false;
+  }
+  insert(v) {
+    if (this.reversed) {
+      this.revert();
+    }
+    if (!this.root) {
+      this.root = new Node(v);
+    } else {
+      this.root.insert(v);
+    }
+    this.size++;
+  }
+  toArray() {
+    return this.root.toArray();
+  }
+  revert() {
+    this.root.revert();
+    this.reversed = !this.reversed;
+  }
+  min() {
+    if (this.reversed) {
+      this.revert();
+    }
+    let node = this.root;
+    let v;
+    while (node) {
+      v = node.value;
+      node = node.left;
+    }
+    return v;
+  }
+  max() {
+    if (this.reversed) {
+      this.revert();
+    }
+    let node = this.root;
+    let v;
+    while (node) {
+      v = node.value;
+      node = node.right;
+    }
+    return v;
+  }
+  maxDepth() {
+    if (this.reversed) {
+      this.revert();
+    }
+
+    function work(node) {
+      if (!node.left && !node.right) {
+        return 0;
+      }
+      if (!node.left && node.right) {
+        return work(node.right) + 1;
+      }
+      if (node.left && !node.right) {
+        return work(node.left) + 1;
+      }
+      if (node.left && node.right) {
+        let m = work(node.left);
+        let n = work(node.right);
+        return m > n ? m + 1 : n + 1;
+      }
+    }
+
+    if (!this.root) {
+      return 0;
+    } else {
+      return work(this.root);
+    }
+  }
+  allPaths() {
+    if (!this.root) {
+      return [];
+    } else {
+      return this.root.allPaths();
+    }
+  }
+  leafNodes() {
+    function work(node) {
+      if (!node.left && !node.right) {
+        return 1;
+      }
+      if (!node.left && node.right) {
+        return work(node.right);
+      }
+      if (node.left && !node.right) {
+        return work(node.left);
+      }
+      return work(node.left) + work(node.right);
+    }
+    if (!this.root) {
+      return 0;
+    } else {
+      // let node = this.root;
+      // let n = 0;
+      return work(this.root);
+    }
+  }
 }
 
 if (typeof module !== 'undefined' && module.parent) {
@@ -150,13 +271,27 @@ if (typeof module !== 'undefined' && module.parent) {
   // t = new BinaryTree(s);
   // console.dir(t._entry, {depth: null});
 
-  let n = new Node(10);
+  let n = new BiTree();
+  console.log('Size: ', n.size);
+  console.log('Max depth: ', n.maxDepth());
+  console.log('All paths:\n', n.allPaths());
+  console.log('Count of leaf nodes: ', n.leafNodes());
   for (let v of s) {
     n.insert(v);
   }
-  console.log(n.toArray());
+  console.log('Size: ', n.size);
+  console.log('Max depth: ', n.maxDepth());
+  console.log('All paths:\n', n.allPaths());
+  console.log('Count of leaf nodes: ', n.leafNodes());
+  // console.log(n.toArray());
+  console.log(n.size);
+  console.log(n.min());
+  console.log(n.max());
   n.revert();
-  console.log(n.toArray());
+  n.insert(99);
+  console.log(n.max());
+  console.log('Max depth: ', n.maxDepth());
+
 
   // console.log(n);
   // n.insert(3);
